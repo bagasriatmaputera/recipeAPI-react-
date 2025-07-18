5e391faa6ecda696fe8d600f5b483b86e898d860
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-07-18 14:57:46 +0700

    don category

---
+++ src/types/type.tsx
@@ -0,0 +1,44 @@
+export interface Recipe {
+    id: number,
+    name: string,
+    slug: string,
+    url_file: string,
+    url_video: string,
+    author: Author,
+    category: Category,
+    thumbnail: string,
+    tutorials: Tutorial[],
+    recipe_ingredients: RecipeIngredient[],
+    about: string
+}
+
+interface Author {
+    id: number,
+    name: string,
+    photo: string
+}
+
+interface Tutorial {
+    id: number,
+    name: string
+}
+
+interface Ingredient {
+    id: number,
+    name: string,
+    photo: string
+}
+
+interface RecipeIngredient {
+    id: number,
+    ingredient: Ingredient
+}
+
+export interface Category {
+    id: number,
+    name: string,
+    icon: string,
+    slug: string,
+    recipe_count: number,
+    recipes: Recipe
+}
